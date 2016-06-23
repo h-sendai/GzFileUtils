2dd2ad2c7cec5708da3c75474026830394d74a36
hiroshi sendai <hiroshi.sendai@kek.jp>
2016-06-23 10:52:36 +0900

    add test routine (file branches)

--- sample.cpp
+++ sample.cpp
@@ -9,10 +9,12 @@ int main(int argc, char *argv[])
     GzFileUtils *g = new GzFileUtils::GzFileUtils();
     
     g->open_file("/tmp/abc");
+    g->set_max_size_in_megaBytes(1);
     
-    char buf[1024];
-    g->write_data(buf, sizeof(buf));
-
+    char buf[1025]; // 1024 + 1 to examine file branches
+    for (int i = 0; i < 10000; ++i) {
+        g->write_data(buf, sizeof(buf));
+    }
 
     g->close_file();
 
